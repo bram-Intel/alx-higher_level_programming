79cef2f418d809e1e0a5b71a803fe24a0f1dcc5b
bram-Intel <abrahamsmoothy41@gmail.com>
2023-02-08 01:19:26 +0100

    'issues'

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
-
-
-import json
+"""defining load_from_json_file function"""
 
 
 import json
 
 def save_to_json_file(my_obj, filename):
+    """writes an object to text file"""
     with open(filename, mode='w') as f:
         json.dump(my_obj, f)
+
+
