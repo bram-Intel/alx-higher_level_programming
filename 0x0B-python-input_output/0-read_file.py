63aa38c77d32a176aba2091622cfa09ba63bd5d8
bram-Intel <abrahamsmoothy41@gmail.com>
2023-02-08 01:04:22 +0100

    'new'

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Documentation for read_file function"""
+
+
+def read_file(filename=""):
+    """Function that reads the file and prints its contents to stdout
+    Args:
+        filename (str): the filename to open
+    """
+
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,2 @@
+input and output
+```
